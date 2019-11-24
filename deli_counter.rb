22c31e52c9556b969af07d6de69dea13bc99daef
Maika Kailani <queenmab051@gmail.com>
2019-11-24 20:23:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
 def line(deli)
   if deli.length == 0
     return "There is nobody in line."
-  return
 end
 
-def take_a_number(deli, name)
-  deli << name
-  puts "Welcome, #{name}. You are number #{deli.length} in line."
-end
+#def take_a_number(deli, name)
+ # deli << name
+  #puts "Welcome, #{name}. You are number #{deli.length} in line."
+#end
