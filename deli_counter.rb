b1505cb349e2eede4b5c54d1d6531668194f8ca2
Maika Kailani <queenmab051@gmail.com>
2019-11-24 20:24:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 def line(deli)
-  #if deli.length == 0
-   # return "There is nobody in line."
+  if deli.length == 0
+    return "There is nobody in line."
+  end
 end
 
 #def take_a_number(deli, name)
