ace885ba330b23b59eadeca1a6776baa182725ba
Maika Kailani <queenmab051@gmail.com>
2019-11-24 20:56:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   else
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     place = 1
     deli.each do |name|
-      current_line << "#{place}. #{name}"
+      current_line << " #{place}. #{name}"
       place += 1
     end
   return current_line
